862a4cb3e4b46269b67b9b55b0ffa9fb5c9792a0
Braden Ganetsky <braden.ganetsky@gmail.com>
2025-04-29 10:09:45 -0500

    Test the tuple

---
+++ c++/src/2025-cppnow/test/tuple_ut.cpp
@@ -0,0 +1,20 @@
+#include "constant.hpp"
+#include "tuple.hpp"
+
+constexpr mguid::tuple<int, double, char> tup{5, 6.0, 'a'};
+
+static_assert(tup.get(0_c) == 5);
+static_assert(tup.get(1_c) == 6.0);
+static_assert(tup.get(2_c) == 'a');
+
+static_assert(std::is_same_v<decltype(auto(tup.get(0_c))), int>);
+static_assert(std::is_same_v<decltype(auto(tup.get(1_c))), double>);
+static_assert(std::is_same_v<decltype(auto(tup.get(2_c))), char>);
+
+namespace {
+
+void compile_test() {
+    auto [a,b,c] = tup;
+}
+
+} // namespace
--- c++/src/2025-cppnow/tuple.hpp
+++ c++/src/2025-cppnow/tuple.hpp
@@ -112,6 +112,12 @@ struct tuple {
         return std::forward_like<Self>(self.data).template get<T, idx>();
     }
 
+    template <std::size_t TIdx, typename Self>
+    constexpr decltype(auto) get(this Self&& self, std::integral_constant<std::size_t, TIdx>) {
+        return std::forward_like<Self>(self.data).template get<
+            typename detail::type_at_index<TIdx, TTypes...>::type, TIdx>();
+    }
+
     constexpr void swap(tuple& other) noexcept
         requires((... && std::is_nothrow_swappable_v<TTypes>))
     {
